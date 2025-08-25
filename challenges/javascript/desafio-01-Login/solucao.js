ff8b9adb9492a1a2bb956435e449f29df2a6a0be
Luanpierote <pieroteluan@gmail.com>
2025-08-25 16:23:09 -0300

    update-3: reformuled func

--- challenges/javascript/desafio-01-Login/solucao.js
+++ challenges/javascript/desafio-01-Login/solucao.js
@@ -2,20 +2,37 @@
 var password = ['1234'];  */
 
 const users = [
-    { login: "andre" , senha: "1234"
+    {
+        login: "andre", senha: "1234"
+    },
+    {
+        login: "ciro", senha: "1234"
+    },
+    {
+        login: "gomes", senha: "1234"
     }
 ]
 
-function loginUser(){
+
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+
+    e.preventDefault(); // bloqueia eventos padrões das tags
 
     var login = document.getElementById("login").value.trim();
     var senha = document.getElementById("senha").value;
 
-    if(login == username && senha == password){
+        const userFound = users.find(u=>u.login === login && u.senha === senha); // método de iteração dos arrays
+        //obs: for(let i in users) também funciona,mas com bugs
+
+    if (userFound) {
         alert("sucesso");
         location.href = "home.html"; //redireciona
-    }else{
-        alert("Usuário ou senha incorretos");
-       /*  mensgErr.style.display = 'block';   FUTURA MENSAGEM DE ERRO  */
-    }
-}
+    } else {
+        /* const mensgErr = document.getElementById('mensagem-erro') FUTURA MENSAGEM DE ERRO */
+alert("Usuário ou senha incorretos");
+        /*  mensgErr.style.display = 'block';   FUTURA MENSAGEM DE ERRO  */
+   }
+
+ }) 
+
+

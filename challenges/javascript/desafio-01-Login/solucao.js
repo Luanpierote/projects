c9216ce87d4adba5e4cd1409e45aab55a1e2f7b9
Luanpierote <pieroteluan@gmail.com>
2025-09-12 20:28:26 -0300

    update-9:Token_base64_login

--- challenges/javascript/desafio-01-Login/solucao.js
+++ challenges/javascript/desafio-01-Login/solucao.js
@@ -3,38 +3,65 @@ var password = ['1234'];  */
 
 const users = [
     {
-        login: "andre", senha: "1234"
+       id:1,  nome: "andre", senha: "1234"
     },
     {
-        login: "ciro", senha: "1234"
+        id:2, nome: "ciro", senha: "1234"
     },
     {
-        login: "gomes", senha: "1234"
+        id:3, nome: "gomes", senha: "1234"
     }
 ]
 
 
+
+
 document.getElementById("loginForm").addEventListener("submit", function (e) {
+
 
     e.preventDefault(); // bloqueia eventos padrões das tags
 
-    var login = document.getElementById("login").value.trim();
+
+    var nome = document.getElementById("login").value.trim();
     var senha = document.getElementById("senha").value;
 
-    const userFound = users.find(u => u.login === login && u.senha === senha); // método de iteração dos arrays
+
+    const userFound = users.find(u => u.nome === nome && u.senha === senha); // método de iteração dos arrays
     //obs: for(let i in users) também funciona,mas com bugs
+
 
     if (userFound) {
         alert("sucesso");
-        location.href = "home.html"; //redireciona
+       
+
+
+        const token = btoa(JSON.stringify({id:userFound.id,name:userFound.nome, exp:Date.now() + 60000})); //simulando a criação de um novo token, utilizando base64
+        console.log("Login ok, token gerado",token);
+       
+       
+   
+        const decode = JSON.parse(atob(token)) //atob - decodifica base64
+
+
+       
+
+
+        return {success: true, token} && console.log(decode);
     } else {
         const mensageErr = document.getElementById("erro");
+
 
         mensageErr.innerHTML = "Usuário ou senha incorretos! Tente Novamente";
         mensageErr.style.display = "block";
         /* const mensgErr = document.getElementById('mensagem-erro') FUTURA MENSAGEM DE ERRO */
         /*  mensgErr.style.display = 'block';   FUTURA MENSAGEM DE ERRO  */
+    return {success:false,message:"Usuário ou senha incorretos! Tente Novamente"}
     }
+
+
+   
+   
 })
+
 
 
